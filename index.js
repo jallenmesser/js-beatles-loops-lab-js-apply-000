2d1a3774fcf3ebc58d7b88399b3279ea80a99940
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-07-21 19:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   let x = []
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 3; i++) {
     x.push(musicians[i] + ' plays ' + instruments[i])
   }
 }
