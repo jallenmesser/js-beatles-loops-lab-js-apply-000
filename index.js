de3cf84422a2c0eeffba13000c2c6721f881e835
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-07-21 19:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians, instruments) {
   let x = []
   for (let i = 0; i < musicians.length; i++) {
-    x.push(musicians[i] + ' plays ' + instruments[i])
+    x.push(musicians[i] + " plays " + instruments[i])
   }
 }
